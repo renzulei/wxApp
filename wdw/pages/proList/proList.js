e858964c4b46e556bf48a6667addf79f7369a6b9
zhangzongbao <402390616@qq.com>
2018-07-05 17:08:45 +0800

    产品列表

--- wdw/pages/proList/proList.js
+++ wdw/pages/proList/proList.js
@@ -5,62 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    showHide: 'true',
+    showHides: 'true'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
+  },
+  screenTap: function() {
+    var that = this;
+    that.setData({
+      showHides: (!that.data.showHides)
+    })
+  },
+
+  // 底部弹窗
+  shoppingTap: function(event) {
+    var that = this;
+    that.setData({
+      showHide: (!that.data.showHide)
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
