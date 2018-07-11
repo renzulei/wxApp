7955160c97273f075d0694a0c718ce56b037a4e3
renzl <1219961802@qq.com>
2018-07-11 13:28:02 +0800

    登录界面模拟数据绑定

--- wdw/pages/login/login.js
+++ wdw/pages/login/login.js
@@ -5,22 +5,63 @@ Page({
    * 页面的初始数据
    */
   data: {
-    Color: "#888"
+    validate: false,//登录名和密码为空检查
+    username: '',
+    password: '',
+    wrong:false,//登录名或密码错误
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
-  },
-  changeColor: function(event) {
-    var bgColor = this.data.Color == '#888' ? 'red' : '#888';
+    var username = wx.getStorageSync('login-username') || '';
+    var password = wx.getStorageSync('login-password') || '';
     this.setData({
-      Color: bgColor
-    });
+      username: username,
+      password: password
+    }, this.checkValidate);
+    if (username != '' && password!=''){
+      // this.login();
+    }
+  },
+  
+  setName: function(e) {
+    var username = e.detail.value;
+    this.setData({
+      username
+    }, this.checkValidate);
+    wx.setStorageSync('login-username', username)
+    
   },
 
+  setPass: function(e) {
+    var password = e.detail.value;
+    this.setData({
+      password
+    }, this.checkValidate);
+    wx.setStorageSync('login-password', password)
+    
+  },
+
+  checkValidate: function() {
+    var bull = false;
+    if (this.data.username != '' && this.data.password != '') {
+      bull = true;
+    }
+    this.setData({
+      validate: bull
+    })
+  },
+
+  login:function(){
+    var username = this.data.username;
+    var password = this.data.password;
+    console.log("登录成功")
+    wx.switchTab({
+      url: '../index/index',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
