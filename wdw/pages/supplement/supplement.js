235cfb81eea30b93f934941a71e8db93bfb29b81
zhangzongbao <402390616@qq.com>
2018-07-17 17:08:21 +0800

    页面的简单逻辑处理

--- wdw/pages/supplement/supplement.js
+++ wdw/pages/supplement/supplement.js
@@ -6,12 +6,53 @@ Page({
    */
   data: {
     showHide: true,
-    list: [
-      {name: "111", value: "标准订单", checkde: true,
-        name:"222", value:"意向订单",
-       },
+    list: [{
+        name: "1",
+        value: "标准订单",
+        checkde: true
+      },
+      {
+        name: "2",
+        value: "意向订单",
+      },
+      {
+        name: "3",
+        value: "下单预付",
+      },
+      {
+        name: "4",
+        value: "发货前预付",
+      },
+      {
+        name: "5",
+        value: "赊销",
+      },
+      {
+        name: "6",
+        value: "30",
+      },
+      {
+        name: "7",
+        value: "60",
+      },
+      {
+        name: "8",
+        value: "120",
+      },
+      {
+        name: "9",
+        value: "现金支付",
+      },
+      {
+        name: "10",
+        value: "白条支付",
+      },
+      {
+        name: "11",
+        value: "票据支付",
+      }
     ],
-    checked: true
+    checked: true,
   },
 
   /**
@@ -20,15 +61,10 @@ Page({
   onLoad: function(options) {
 
   },
-
-  radioChange: function(e) {
-      console.log("点击事件");
-      var that = this;
-      var checked = this.data.checked;
-      console.log(checked)
-      that.setData({
-        checked: (!that.data.checked)
-      })
+  returnFor: function(e) {
+    wx.navigateBack({
+      delta: 0
+    })
   },
 
   // 点击小图标商品列表显示和隐藏
@@ -39,11 +75,12 @@ Page({
     })
   },
 
-  succeedTap: function() {
+  succeedTap: function(e) {
     wx.navigateTo({
       url: '/pages/affirm/affirm',
     })
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
