1a28f4081b3208759838b28b96971fa9752a4134
renzl <1219961802@qq.com>
2018-07-26 11:55:40 +0800

    首页修改

--- wdw/pages/index/index.js
+++ wdw/pages/index/index.js
@@ -11,19 +11,30 @@ Page({
   data: {
     bgc: "/images/bgc.png",
     searchValue: '',
-    data_List: [],
-    info: [],
-    article: []
+    businessExplain:[],
+    article: [],
+    data: {}, //商品请求接口时携带数据
+    shop:[] //商品列表数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    // 促销商品
-    this.dataInof();
-    // 供应链
-    this.getInof();
+    var userDefaultTradeCompany = util.getStorageSync('userDefaultTradeCompany');
+    var partyName = userDefaultTradeCompany ? userDefaultTradeCompany.partyName : "";
+    var address = userDefaultTradeCompany ? userDefaultTradeCompany.address : "";
+    var tradePartyId = userDefaultTradeCompany ? userDefaultTradeCompany.tradePartyId : "";
+    var contactId = userDefaultTradeCompany ? userDefaultTradeCompany.contactId : "";
+    this.setData({
+      partyName: partyName,
+      address: address,
+      tradePartyId: tradePartyId,
+      contactId: contactId
+    })
+   
+    this.dataInof(); // 促销商品
+    this.getInof();// 供应链
   },
   // 搜索框键盘输入值处理函数
   upSeachValue: function(e) {
@@ -43,31 +54,74 @@ Page({
 
   //促销商品数据
   dataInof: function() {
+    var data = this.data.data
+    data.saleCountSort = this.data.saleCountSortName || ''
+    data.priceSort = this.data.priceSortName || ''
+    //获取商品数据
+    if (this.data.searchValue) {
+      data.searchBoxContext = this.data.searchValue;
+    }
+    this.fetch({
+      pageSize: 10,
+      current: 1,
+      data: data
+    })
+  },
+  // 根据筛选条件请求商品列表数据
+  fetch: function(params = {}) {
+    // console.log(params)
     var that = this;
+    var tradePartyId = this.data.tradePartyId;
+    var contactId = this.data.contactId;
+    wx.showLoading({
+      title: "加载中...",
+      mask: true
+    })
     wx.request({
-      url: `${cmService}/businessExplain/query?region=ZYCP`,
-      data: {},
+      url: `${cmService}/product/getProductByCondition?page=${params.current}&pageSize=${params.pageSize}&tradePartyId=${tradePartyId}&contactId=${contactId}`,
       method: 'POST',
       header: {
         'Content-Type': 'application/json',
-        'cookie': '__wgl=' + wx.getStorageSync('__wgl')
+        'cookie': util.noLoginCookie
+      },
+      data: JSON.stringify(params.data),
+      complete: function() {
+        wx.hideLoading()
       },
       success: function(res) {
-        var list = res.data;
-        console.log(list);
-        that.setData({
-          title: res.data.categoryName,
-          data_List: list
+        try {
+          util.catchHttpError(res);
+        } catch (e) {
+          console.error(e)
+          return
+        }
+        var data = res.data;
+        let shop = [];
+        data.content.map((item, i) => {
+          var item = JSON.parse(item)
+          item.key = i
+          if (item.itemAttributes) {
+            item.itemAttributes.map((it, i) => {
+              if (it.attributeCode == "Y033")
+                item.fixedAttributeValue = it.fixedAttributeValue
+            })
+          }
+          shop.push(item)
         })
-      },
+        console.log(shop)
+        that.setData({
+          shop: shop,
+          total: data.total
+        })
+      }
     })
   },
-
   // 供应链数据
   getInof: function() {
     var that = this;
     wx.request({
       url: `${cmService}/businessExplain/query?region=GYLJR`,
+      // url: `https://yz.wangreat.com/core/restapi/public/businessExplain/query?region=GYLJR`,
       data: {},
       method: 'POST',
       header: {
@@ -75,19 +129,29 @@ Page({
         'cookie': '__wgl=' + wx.getStorageSync('__wgl')
       },
       success: function(res) {
-        var infoData = res.data;
-        // console.log(res.data.businessExplain[0].simpleContent)
-        // console.log(infoData);
+        try {
+          util.catchHttpError(res);
+        } catch (e) {
+          console.error(e)
+          return
+        }
+        var json = res.data;
+        // console.log(res);
         that.setData({
-          title_name: res.data.categoryName,
-          info: infoData,
+          businessExplain: json.businessExplain,
+          categoryName: json.categoryName,
+          businessIconUrl: json.businessIconUrl,
         })
-
-        //底部图片
-          var article = that.data.article;
-         article = res.data.businessExplain[0].simpleContent;
-        WxParse.wxParse('article', 'html', article, that,);
-        
+        var replyArr = [];
+        json.businessExplain.map(function(item,i){
+          replyArr.push(item.simpleContent);
+        })
+        for (let i = 0; i < replyArr.length; i++) {
+          WxParse.wxParse('reply' + i, 'html', replyArr[i], that);
+          if (i === replyArr.length - 1) {
+            WxParse.wxParseTemArray("replyTemArray", 'reply', replyArr.length, that)
+          }
+        }
       }
     })
   },
