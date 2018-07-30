de5bb84f03b2d2624267b8b728d96f75836973d2
renzl <1219961802@qq.com>
2018-07-30 17:06:19 +0800

    购物车第一步渲染

--- wdw/pages/createOrder/createOrder.js
+++ wdw/pages/createOrder/createOrder.js
@@ -1,22 +1,27 @@
 // pages/createOrder/createOrder.js
+var app = getApp();
+var cmService = app.globalData.cmService;
+var authService = app.globalData.authService;
+var customer_id = app.globalData.customer_id;
+const util = require('../../utils/util.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   // 点击取消返回
   cancelTap: function(e) {
-     wx.navigateBack({
-       delta: 1
-     })
+    wx.navigateBack({
+      delta: 1
+    })
   },
 
   //  跳转到补充信息页面
-  sopTap: function () {
+  sopTap: function() {
     wx.navigateTo({
       url: '/pages/supplement/supplement',
     })
@@ -24,56 +29,133 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    let authorizedCookie = encodeURI("__wgt=" + util.getStorageSync('__wgt') + ";" + "__wgl=" + util.getStorageSync('__wgl') + ";" + "menuKey=" + util.getStorageSync('menuKey') + ";" + "userName=" + util.getStorageSync('userName') + ";" + 'userDefaultTradeCompany=' + JSON.stringify(util.getStorageSync('userDefaultTradeCompany')));
+    this.setData({
+      authorizedCookie: authorizedCookie
+    })
+    var userDefaultTradeCompany = util.getStorageSync('userDefaultTradeCompany');
+    var partyName = userDefaultTradeCompany ? userDefaultTradeCompany.partyName : "";
+    var address = userDefaultTradeCompany ? userDefaultTradeCompany.address : "";
+    var tradePartyId = userDefaultTradeCompany ? userDefaultTradeCompany.tradePartyId : "";
+    var contactId = userDefaultTradeCompany ? userDefaultTradeCompany.contactId : "";
+    this.setData({
+      partyName: partyName,
+      address: address,
+      tradePartyId: tradePartyId,
+      contactId: contactId
+    }, this.getData)
   },
-
+  getData: function() {
+    wx.request({
+      url: `${authService}/shopCart/showShopCart?tradePartyId=${this.data.tradePartyId}`,
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json',
+        'cookie': this.data.authorizedCookie
+      },
+      success: (res)=> {
+        // console.log(res.data)
+        try {
+          util.catchHttpError(res);
+        } catch (e) {
+          console.error(e)
+          return
+        }
+        var json = res.data;
+        var contact = {}
+        var defaultContactPerson = {}
+        json.itemList.map((items, i) => {
+          this.setData({
+            contactId: items.contact.contactId,
+            tradePartyId: items.contact.tradePartyId
+          })
+          contact = items.contact
+          defaultContactPerson = contact.defaultContactPerson
+          items.items.map((its) => {
+            its.contactId = contact.contactId,
+              its.address = contact.address,
+              its.tradePartyId = contact.tradePartyId
+          })
+        })
+        json.itemList.map((item, i) => {
+          let arr = [];
+          if (item.items)
+            item.items.map((data, i) => {
+              data.itemChangableAttributes.map((item) => {
+                data[item["attributeCode"]] = item.value;
+                if (item["attributeName"] == '幅宽mm'){
+                  this.setData({
+                    code: item["attributeCode"]
+                  })
+                }else{
+                  this.setData({
+                    cutLength: item["attributeCode"]
+                  })
+                } 
+              })
+              data.key = i + 1;
+              arr.push(data)
+            })
+          this.setData({
+            tableSource:arr //表格数据源
+          })
+          console.log(arr)
+        })
+        this.setData({
+          data: json.itemList,
+          contact: contact,
+          defaultContactPerson: defaultContactPerson, //默认收货人
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
