124a2408f8fdddfa6a0dc42c743ec564b4686915
renzl <1219961802@qq.com>
2018-08-02 17:27:40 +0800

    创建订单页件数和吨数修改函数，能否跳转判断

--- wdw/pages/createOrder/createOrder.js
+++ wdw/pages/createOrder/createOrder.js
@@ -18,20 +18,7 @@ Page({
     selectedRows: [], //被选中的商品数据
     allA: 0, //总金额
     allT: 0, //总吨数
-  },
-
-  // 点击取消返回
-  cancelTap: function(e) {
-    wx.navigateBack({
-      delta: 1
-    })
-  },
-
-  //  跳转到补充信息页面
-  sopTap: function() {
-    wx.navigateTo({
-      url: '/pages/supplement/supplement',
-    })
+    jump:true,//是否跳到下一个页面
   },
   /**
    * 生命周期函数--监听页面加载
@@ -63,7 +50,7 @@ Page({
         'cookie': this.data.authorizedCookie
       },
       success: (res) => {
-        // console.log(res.data)
+        console.log(res.data)
         try {
           util.catchHttpError(res);
         } catch (e) {
@@ -146,9 +133,6 @@ Page({
   // 选中一类商品
   checkSome: function(e) {
     var outIndex = e.currentTarget.dataset.outindex;
-    var selectedRows = [],
-      allA = 0,
-      allT = 0;
     this.data.tableSource.map(function(item, index) {
       if (index == outIndex) {
         item[0].checked = !item[0].checked;
@@ -157,30 +141,15 @@ Page({
         })
       }
     })
-    this.data.tableSource.map(function(item, index) {
-      item[1].map(function(it, i) {
-        it.checked ? selectedRows.push(it) : '';
-      })
-    })
-    selectedRows.map(function(it, i) {
-      allA += Number(it.amount);
-      allT += Number(it.baseOrderQuantity);
-    })
-    allT = allT.toFixed(3) //吨数保留三位小数
+    this.countSelected(this.data.tableSource);
     this.setData({
       tableSource: this.data.tableSource,
-      selectedRows: selectedRows,
-      allA: allA,
-      allT: allT
     })
   },
   // 选中一个商品
   checkOne: function(e) {
     var outIndex = e.currentTarget.dataset.outindex;
     var innerIndex = e.currentTarget.dataset.index;
-    var selectedRows = [],
-      allA = 0,
-      allT = 0;
     this.data.tableSource.map(function(item, index) {
       if (index == outIndex) {
         item[0].checked = true;
@@ -190,19 +159,27 @@ Page({
         })
       }
     })
-    this.data.tableSource.map(function(item, index) {
+    this.countSelected(this.data.tableSource);
+    this.setData({
+      tableSource: this.data.tableSource,
+    })
+  },
+  // 计算被选中的商品
+  countSelected: function(tableSource) {
+    var selectedRows = [];
+    var allA = 0,
+      allT = 0;
+    tableSource.map(function(item, index) {
       item[1].map(function(it, i) {
         it.checked ? selectedRows.push(it) : '';
       })
     })
-    // console.log(selectedRows)
     selectedRows.map(function(it, i) {
       allA += Number(it.amount);
       allT += Number(it.baseOrderQuantity);
     })
     allT = allT.toFixed(3) //吨数保留三位小数
     this.setData({
-      tableSource: this.data.tableSource,
       selectedRows: selectedRows,
       allA: allA,
       allT: allT
@@ -245,6 +222,278 @@ Page({
           this.getData()
           this.setData({
             selectedRows: []
+          })
+        }
+      }
+    })
+  },
+  // 件数改变触发事件
+  orderQuantityChange: function(e) {
+    var value = e.detail.value;
+    var innerIndex = e.currentTarget.dataset.index;
+    var outIndex = e.currentTarget.dataset.outindex;
+    this.data.tableSource.map(function(item, index) {
+      if (index == outIndex) {
+        item[1].map(function(it, i) {
+          i == innerIndex ? it.orderQuantity = isNaN(Number(value).toFixed(0)) ? "" : Number(value).toFixed(0) : '';
+        })
+      }
+    })
+    this.setData({
+      tableSource: this.data.tableSource,
+    })
+  },
+  // 件数获得焦点事件
+  orderQuantityFocus: function(e) {
+    var value = e.detail.value,
+      orderQuantityValue; //临时变量，用于记录获取焦点时的初始值。当离开焦点时，如果输入框的值等于初始值则不发出请求
+    if (!value) {
+      orderQuantityValue = ""
+    } else {
+      orderQuantityValue = value
+    }
+    this.setData({
+      orderQuantityValue: orderQuantityValue
+    })
+  },
+  // 件数离开焦点触发事件
+  orderQuantityBlur: function(e) {
+    var value = e.detail.value;
+    var innerIndex = e.currentTarget.dataset.index;
+    var outIndex = e.currentTarget.dataset.outindex;
+    var record, code = this.data.code,
+      cutLength = this.data.cutLength;
+    this.data.tableSource.map(function(item, index) {
+      if (index == outIndex) {
+        item[1].map(function(it, i) {
+          i == innerIndex ? record = it : '';
+        })
+        if (!record.orderQuantity) {
+          record.orderQuantity = ""
+        }
+      }
+    })
+    if (record.physicalStateCode == "JT") {
+      if (this.data.orderQuantityValue != value)
+        wx.request({
+          url: `${authService}/common/getItemTonQuantity?itemId=${record.itemId}&pieceQty=${Number(record.orderQuantity).toFixed(0)}&rangeWidth=${record[code]}`,
+          method: 'POST',
+          header: {
+            'Content-Type': 'application/json',
+            'cookie': this.data.authorizedCookie
+          },
+          success: (res) => {
+            console.log(res.data)
+            try {
+              util.catchHttpError(res);
+            } catch (e) {
+              console.error(e)
+              return
+            }
+            var json = res.data;
+            if (json) {
+              this.data.tableSource.map(function(item, index) {
+                if (index == outIndex) {
+                  item[1].map(function(it, i) {
+                    if (i == innerIndex) {
+                      it.baseOrderQuantity = (Number(json)).toFixed(3)
+                      it.amount = (Number(json) * Number(it.unitPrice)).toFixed(2);
+                    }
+                  })
+                }
+              })
+
+            } else {
+              this.data.tableSource.map(function(item, index) {
+                if (index == outIndex) {
+                  item[1].map(function(it, i) {
+                    if (i == innerIndex) {
+                      it.baseOrderQuantity = 0
+                      it.amount = 0;
+                    }
+                  })
+                }
+              })
+            }
+            this.countSelected(this.data.tableSource);
+            this.setData({
+              tableSource: this.data.tableSource,
+            })
+          }
+        })
+    } else if (record.physicalStateCode == "PZ") {
+      if (this.data.orderQuantityValue != value)
+        wx.request({
+          url: `${authService}/common/getItemPZTonQuantity?itemId=${record.itemId}&pieceQty=${Number(record.orderQuantity)}&rangeWidth=${record[code]}&cutLength=${record[cutLength]}`,
+          method: 'POST',
+          header: {
+            'Content-Type': 'application/json',
+            'cookie': this.data.authorizedCookie
+          },
+          success: (res) => {
+            console.log(res.data)
+            try {
+              util.catchHttpError(res);
+            } catch (e) {
+              console.error(e)
+              return
+            }
+            var json = res.data;
+            if (json) {
+              this.data.tableSource.map(function(item, index) {
+                if (index == outIndex) {
+                  item[1].map(function(it, i) {
+                    if (i == innerIndex) {
+                      it.baseOrderQuantity = (Number(json)).toFixed(3)
+                      it.amount = (Number(json) * Number(it.unitPrice)).toFixed(2);
+                    }
+                  })
+                }
+              })
+
+            } else {
+              this.data.tableSource.map(function(item, index) {
+                if (index == outIndex) {
+                  item[1].map(function(it, i) {
+                    if (i == innerIndex) {
+                      it.baseOrderQuantity = ''
+                      it.amount = 0;
+                    }
+                  })
+                }
+              })
+            }
+            this.countSelected(this.data.tableSource);
+            this.setData({
+              tableSource: this.data.tableSource,
+            })
+          }
+        })
+    }
+  },
+  // 吨数改变触发事件
+  baseOrderQuantityChange: function(e) {
+    var value = e.detail.value;
+    var innerIndex = e.currentTarget.dataset.index;
+    var outIndex = e.currentTarget.dataset.outindex;
+    if (value) {
+      this.data.tableSource.map(function(item, index) {
+        if (index == outIndex) {
+          item[1].map(function(it, i) {
+            if (i == innerIndex) {
+              if (value != it.baseOrderQuantity) {
+                it.orderQuantity = ''
+              }
+              it.baseOrderQuantity = value;
+            }
+          })
+        }
+      })
+    } else {
+      this.data.tableSource.map(function(item, index) {
+        if (index == outIndex) {
+          item[1].map(function(it, i) {
+            if (i == innerIndex) {
+              it.baseOrderQuantity = 0;
+            }
+          })
+        }
+      })
+    }
+    this.setData({
+      tableSource: this.data.tableSource,
+    })
+  },
+  // 吨数离开焦点触发事件
+  baseOrderQuantityBlur: function(e) {
+    var value = e.detail.value;
+    var innerIndex = e.currentTarget.dataset.index;
+    var outIndex = e.currentTarget.dataset.outindex;
+    this.data.tableSource.map(function(item, index) {
+      if (index == outIndex) {
+        item[1].map(function(it, i) {
+          if (i == innerIndex) {
+            it.baseOrderQuantity = Number(value).toFixed(3);
+            it.amount = (Number(it.baseOrderQuantity) * Number(it.unitPrice)).toFixed(2);
+          }
+        })
+      }
+    })
+    this.countSelected(this.data.tableSource);
+    this.setData({
+      tableSource: this.data.tableSource,
+    })
+  },
+
+  // 点击取消返回
+  cancelTap: function(e) {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+
+  //  跳转到补充信息页面
+  sopTap: function() {
+    var jump = true;
+    if (this.data.selectedRows.length != 0){
+      this.data.selectedRows.map((it)=>{
+        if (!Number(it.baseOrderQuantity)) {
+          wx.showToast({
+            title: '吨数不能为0！',
+            icon: 'none',
+            duration: 1000
+          })
+          jump = false
+        } else if (!Number(it.amount)) {
+          wx.showToast({
+            title: '金额不能为0！',
+            icon: 'none',
+            duration: 1000
+          })
+          jump = false
+        }else{
+          this.checkUserFreeze()
+        }
+      })
+      if(jump&&this.data.jump){
+        wx.navigateTo({
+          url: '/pages/supplement/supplement',
+        })
+      }
+    } else {
+      wx.showToast({
+        title: '请选择要提交的行数据！',
+        icon: 'none',
+        duration: 1000
+      })
+    }
+    
+  },
+  checkUserFreeze: function() {
+    wx.request({
+      url: `${authService}/saleOrder/checkUserFreeze?tradePartyId=${this.data.tradePartyId}`,
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json',
+        'cookie': this.data.authorizedCookie
+      },
+      success: (res) => {
+        console.log(res.data)
+        try {
+          util.catchHttpError(res);
+        } catch (e) {
+          console.error(e)
+          return
+        }
+        var json = res.data;
+        if (json.code != "S") {
+          wx.showToast({
+            title: json.msg,
+            icon: 'none',
+            duration: 1000
+          })
+          this.setData({
+            jump:false
           })
         }
       }
